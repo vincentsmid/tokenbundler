e04cb1f1141759dfb14abb3922f33456cbc18ddb
vincentsmid <vincent.smid@seznam.cz>
2024-04-05 23:42:07 +0200

    🏷️ Update schemas

---
+++ main_app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field, StrictBool, StrictInt, StrictStr, EmailStr
+
+class IncomingWalletRequestType(BaseModel):
+    wallet_id: StrictStr = Field(..., title="Wallet ID")
+    chain: StrictStr = Field(..., title="Chain")
+
+class TokenInformationType(BaseModel):
+    amount: StrictInt = Field(..., title="Amount")
+    token_id: StrictInt = Field(..., title="Token ID")
+    token_address: StrictStr = Field(..., title="Token Address")
+    contract_type: StrictStr = Field(..., title="Contract Type")
+    block_number: StrictInt = Field(..., title="Block Number")
+    
